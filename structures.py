936429db055d9d65331f347066086aca765e4b07
Kaj Bostrom <kajb@allenai.org>
2018-10-24 16:39:22 -0700

    shtuff

---
+++ structures.py
@@ -0,0 +1,8 @@
+
+from fractions import Fraction
+
+class Context:
+    def __init__(self, tempo, fundamental_frequency):
+        self.tempo = tempo # in beats/min
+        self.fundamental_frequency = fundamental_frequency # in Hz
+
