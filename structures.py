3ad634da3118ad528c1459f626a77bf86bd7a4b1
Kaj Bostrom <kajb@allenai.org>
2018-10-25 16:32:44 -0700

    add Take method to Duration

--- structures.py
+++ structures.py
@@ -3,7 +3,8 @@ from fractions import Fraction
 from typing import Any
 
 class Duration:
-    pass
+    def take(self, amount: Fraction) -> Duration:
+        raise NotImplementedError
 
 class Infinite(Duration):
     def __gt__(self, value):
@@ -16,6 +17,8 @@ class Infinite(Duration):
         return False
     def __eq__(self, value):
         return False
+    def take(self, amount: Fraction) -> Duration:
+        return Finite(amount)
 
 class Finite(Duration):
     def __init__(self, length: Fraction):
@@ -30,4 +33,10 @@ class Finite(Duration):
         return isinstance(other, Infinite) or self.length <= other.length
     def __eq__(self, value):
         return isinstance(other, Finite) and self.length == other.length
+    def take(self, amount: Fraction) -> Duration:
+        if amount > self.length:
+            raise ValueError
+        else:
+            self.length -= amount
+            return Finite(amount)
 
