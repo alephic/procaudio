2b39f9eb831214311a5982a63bce1a2ef9864830
Kaj Bostrom <kajb@allenai.org>
2019-02-27 17:36:31 -0800

    update structures, add linear decay envelope

--- structures.py
+++ structures.py
@@ -1,14 +1,35 @@
 
 from fractions import Fraction
-from typing import Any, Set, Mapping
+from typing import Any, Set, Iterable, Tuple
 
 Time = Fraction
 Duration = Fraction
 
-class Note:
-    def __init__(self, key: int)
+Track = Iterable[Tuple[Set[Node], Duration]]
 
-Track = Mapping[Time, Note]
+class Note:
+    def __init__(self, key: int, instrument):
+        self.key = key
+        self.instrument = instrument
+
+class Span:
+    def __init__(self, notes: Set[Note], length: Duration):
+        self.notes = notes
+        self.length = length
+    def traverse(self) -> Track:
+        yield (self.notes, self.duration)
+    def get_length(self) -> Duration:
+        return self.length
+
+class Superspan(Span):
+    def __init__(self, subspans: Iterable[Span]):
+        self.length = sum(subspan.get_length() for subspan in subspans)
+        self.subspans = subspans
+    def traverse(self) -> Track:
+        for subspan in self.subspans:
+            yield from subspan.traverse()
+    def get_length(self) -> Duration:
+        return self.length
 
 def compose(seed, levels: int):
     
--- synths.py
+++ synths.py
@@ -6,3 +6,10 @@ def sin(freq):
 
 def noise(t):
     return np.random.normal(scale=0.5, size=t.shape)
+
+def linear_decay(length):
+    def f(t):
+        x = 1.0 - t/length
+        return np.clip(x, 0.0, 1.0, out=x)
+    return f
+
