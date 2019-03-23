d2af55d995bce7714e57364a6c9cafb417164fb5
Kaj Bostrom <bostromk@cs.washington.edu>
2019-03-22 20:31:16 -0700

    remove silly envelope

--- synths.py
+++ synths.py
@@ -6,10 +6,3 @@ def sin(freq):
 
 def noise(t):
     return np.random.normal(scale=0.5, size=t.shape)
-
-def linear_decay(length):
-    def f(t):
-        x = 1.0 - t/length
-        return np.clip(x, 0.0, 1.0, out=x)
-    return f
-
