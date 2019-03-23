30090fa66ff120397526f98a096ee0d346092fe4
Kaj Bostrom <bostromk@cs.washington.edu>
2019-03-22 20:36:16 -0700

    remove spans

--- structures.py
+++ structures.py
@@ -1,35 +1,9 @@
 
 from fractions import Fraction
-from typing import Any, Set, Iterable, Tuple
+from typing import Any, Set, Iterable, Tuple, Mapping
 
 Time = Fraction
 Duration = Fraction
+Note = int
 
-Track = Iterable[Tuple[Set[Node], Duration]]
-
-class Note:
-    def __init__(self, key: int, instrument):
-        self.key = key
-        self.instrument = instrument
-
-class Span:
-    def __init__(self, notes: Set[Note], length: Duration):
-        self.notes = notes
-        self.length = length
-    def traverse(self) -> Track:
-        yield (self.notes, self.duration)
-    def get_length(self) -> Duration:
-        return self.length
-
-class Superspan(Span):
-    def __init__(self, subspans: Iterable[Span]):
-        self.length = sum(subspan.get_length() for subspan in subspans)
-        self.subspans = subspans
-    def traverse(self) -> Track:
-        for subspan in self.subspans:
-            yield from subspan.traverse()
-    def get_length(self) -> Duration:
-        return self.length
-
-def compose(seed, levels: int):
-    
+Track = Mapping[Time, Set[Note]]
