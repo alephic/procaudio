c12621a82b43a0b699c4862261381ad4d39622d8
Kaj Bostrom <bostromk@cs.washington.edu>
2019-06-17 23:42:20 -0700

    get less obsessed with typing

--- structures.py
+++ structures.py
@@ -1,20 +1,13 @@
 
-from fractions import Fraction
-from typing import Any, Set, Iterable, Tuple, Mapping
-
-Time = Fraction
-Duration = Fraction
-
 class Note:
-    def __init__(self, key: int):
+    def __init__(self, key, time):
         self.key = key
+        self.time = time
 
 class HeldNote(Note):
-    def __init__(self, key: int, length: Duration):
-        super().__init__(key)
+    def __init__(self, key, time, length):
+        super().__init__(key, time)
         self.length = length
-
-Track = Mapping[Time, Note]
 
 # composition procedure:
 # start with a base position (time 0, metric level 0)
