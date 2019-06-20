0ef0df4a8ab396ac4dabdf8ee43fe5f2e148a19d
Kaj Bostrom <bostromk@cs.washington.edu>
2019-06-19 23:59:08 -0700

    add some structure stuff

--- structures.py
+++ structures.py
@@ -1,19 +1,45 @@
 
+from fractions import Fraction as F
+
 class Note:
     def __init__(self, key, sus):
         self.key = key
         self.sus = sus
+    def __repr__(self):
+        return f'Note({self.key}, {self.sus})'
+
+class TimedNote(Note):
+    def __init__(self, key, sus, time):
+        super().__init__(key, sus)
+        self.time = time
+    @classmethod
+    def from_note(cls, note, time):
+        return cls(note.key, note.sus, time)
+    def __repr__(self):
+        return f'TimedNote({self.key}, {self.sus}, {self.time})'
+
+class TimeTracker:
+    def __init__(self, time=0):
+        self.time = time
 
 class Span:
     def __init__(self, length, children):
         self.length = length
         self.children = children
-    def __iter__(self):
+    def unroll(self, time_tracker=None):
+        time_tracker = time_tracker or TimeTracker()
         for child in self.children:
             if isinstance(child, Note):
-                yield child
+                yield TimedNote.from_note(child, time_tracker.time)
             else:
-                yield from iter(child)
+                yield from child.unroll(time_tracker)
+        time_tracker.time += self.length
 
-def compose(scale, min_meter_level):
-    
+class Position:
+    def __init__(self, time, meter_level):
+        self.time = time
+        self.meter_level = meter_level
+    def __hash__(self):
+        return hash(self.time) ^ hash(self.meter_level)
+    def __eq__(self, other):
+        return self.time == other.time and self.meter_level == other.meter_level
